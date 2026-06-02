26ba8fdd653ce5cc7cb75f1a65a9a313a394fb00
goupengnb <105686737+goupengnb@users.noreply.github.com>
2026-06-02 11:49:53 +0800

    更新 Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -32,9 +32,6 @@ UPDATE_PACKAGE() {
 
 	# 克隆 GitHub 仓库
 	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
-	
-    #adguardhome
-    git clone --depth=1 --single-branch https://github.com/rufengsuixing/luci-app-adguardhome.git
 
 	# 处理克隆的仓库
 	if [[ "$PKG_SPECIAL" == "pkg" ]]; then
