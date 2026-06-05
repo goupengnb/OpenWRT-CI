236fa6158060b63880026dd9a79d39cf67d78f13
goupengnb <105686737+goupengnb@users.noreply.github.com>
2026-06-05 18:48:41 +0800

    更新 Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -5,19 +5,21 @@
 
 
 
-echo "====【HANDLES调试启动】当前路径:$(pwd)===="
-# 测试1：判断ADG插件目录是否存在
-ADG_CHECK=feeds/luci/applications/luci-app-adguardhome
-if [ -d ${ADG_CHECK} ];then
-    echo "✅【调试】ADG插件目录真实存在:${ADG_CHECK}"
-    # 在插件root/bin生成标记文件
-    TEST_DIR=${ADG_CHECK}/root/usr/bin
-    mkdir -p ${TEST_DIR}
-    echo "test_write_ok" > ${TEST_DIR}/debug.tag
-    echo "✅【调试】已写入标记文件:${TEST_DIR}/debug.tag"
+echo "====【HANDLES调试】PWD=$(pwd)===="
+cd ../
+echo "退回后PWD=$(pwd)"
+TARGET=feeds/luci/applications/luci-app-adguardhome
+if [ -d $TARGET ];then
+    echo "✅存在目录:$TARGET"
+    mkdir -p $TARGET/root/usr/bin
+    echo ok > $TARGET/root/usr/bin/test.tag
+    echo "✅写入test.tag完成"
 else
-    echo "❌【调试】ADG插件目录不存在"
+    echo "❌目录不存在:$TARGET"
 fi
+
+
+
 
 
 
