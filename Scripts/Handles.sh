2584de554d7064073daf5debf88b6a563273d8f6
goupengnb <105686737+goupengnb@users.noreply.github.com>
2026-06-05 18:29:44 +0800

    更新 Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -2,6 +2,26 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2026 VIKINGYFY
 
+
+
+
+echo "====【HANDLES调试启动】当前路径:$(pwd)===="
+# 测试1：判断ADG插件目录是否存在
+ADG_CHECK=feeds/luci/applications/luci-app-adguardhome
+if [ -d ${ADG_CHECK} ];then
+    echo "✅【调试】ADG插件目录真实存在:${ADG_CHECK}"
+    # 在插件root/bin生成标记文件
+    TEST_DIR=${ADG_CHECK}/root/usr/bin
+    mkdir -p ${TEST_DIR}
+    echo "test_write_ok" > ${TEST_DIR}/debug.tag
+    echo "✅【调试】已写入标记文件:${TEST_DIR}/debug.tag"
+else
+    echo "❌【调试】ADG插件目录不存在"
+fi
+
+
+
+
 PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
 
